c69d26e165538f224e79d63c25a3b679c0d32980
Ythosa <vasus714@yandex.ru>
2021-09-14 15:19:08 +0300

    fix: naming

--- 1.introduction/task_1.c
+++ 1.introduction/task_1.c
@@ -2,33 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void calculate_incomplete_quotient(int dividend, int divisor, int* quotient, int* reminder)
+void calculateIncompleteQuotient(int dividend, int divisor, int* quotient, int* reminder)
 {
     *quotient = 0;
     *reminder = 0;
 
     short sign = (dividend < 0 ^ divisor < 0) ? -1 : 1;
-    int dividend_abs = abs(dividend);
-    int divisor_abs = abs(divisor);
+    int dividendAbs = abs(dividend);
+    int divisorAbs = abs(divisor);
 
-    while (dividend_abs >= divisor_abs) {
-        dividend_abs -= divisor_abs;
+    while (dividendAbs >= divisorAbs) {
+        dividendAbs -= divisorAbs;
         *quotient += 1;
     }
 
     if (dividend > 0) {
-        *reminder = dividend - *quotient * divisor_abs;
+        *reminder = dividend - *quotient * divisorAbs;
         *quotient = *quotient * sign;
 
         return;
     }
 
-    int quotient_for_reminder = -*quotient - 1;
-    *reminder = dividend - divisor * quotient_for_reminder;
-    *quotient = abs(quotient_for_reminder) * sign;
+    int quotientForReminder = -*quotient - 1;
+    *reminder = dividend - divisor * quotientForReminder;
+    *quotient = abs(quotientForReminder) * sign;
 }
 
-bool get_dividend_and_divisor(int* dividend, int* divisor)
+bool getDividendAndDivisor(int* dividend, int* divisor)
 {
     printf("Input dividend:");
     scanf("%d", dividend);
@@ -48,13 +48,13 @@ int main()
 {
     int dividend = 0;
     int divisor = 0;
-    if (!get_dividend_and_divisor(&dividend, &divisor)) {
+    if (!getDividendAndDivisor(&dividend, &divisor)) {
         return -1;
     }
 
     int quotient = 0;
     int reminder = 0;
-    calculate_incomplete_quotient(dividend, divisor, &quotient, &reminder);
+    calculateIncompleteQuotient(dividend, divisor, &quotient, &reminder);
 
     printf("Results:\n\tquotient: %d\n\treminder: %d\n", quotient, reminder);
 
