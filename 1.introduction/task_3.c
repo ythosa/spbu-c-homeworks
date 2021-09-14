aa82595c623e5dfe285675c2c3565e96bdf2a5cf
Ythosa <vasus714@yandex.ru>
2021-09-14 15:23:43 +0300

    fix: naming

--- 1.introduction/task_3.c
+++ 1.introduction/task_3.c
@@ -1,44 +1,44 @@
 #include <malloc.h>
 #include <stdio.h>
 
-void reverse_array(int first_index, int size, int* array)
+void reverseArray(int firstIndex, int size, int* array)
 {
     int temp;
     for (int i = 0; i < size / 2; i++) {
-        temp = array[size - i - 1 + first_index];
-        array[size - i - 1 + first_index] = array[i + first_index];
-        array[i + first_index] = temp;
+        temp = array[size - i - 1 + firstIndex];
+        array[size - i - 1 + firstIndex] = array[i + firstIndex];
+        array[i + firstIndex] = temp;
     }
 }
 
-void swap_array_parts(int left_part_size, int right_part_size, int* array)
+void swapArrayParts(int leftPartSize, int rightPartSize, int* array)
 {
-    reverse_array(0, left_part_size, array);
-    reverse_array(left_part_size, right_part_size, array);
-    reverse_array(0, left_part_size + right_part_size, array);
+    reverseArray(0, leftPartSize, array);
+    reverseArray(leftPartSize, rightPartSize, array);
+    reverseArray(0, leftPartSize + rightPartSize, array);
 }
 
 int main()
 {
-    int left_part_size = 0;
-    printf("Input m (left_part_size)=");
-    scanf("%d", &left_part_size);
+    int leftPartSize = 0;
+    printf("Input m (left part size)=");
+    scanf("%d", &leftPartSize);
 
-    int right_part_size = 0;
-    printf("Input n (right_part_size)=");
-    scanf("%d", &right_part_size);
+    int rightPartSize = 0;
+    printf("Input n (right part size)=");
+    scanf("%d", &rightPartSize);
 
-    int array_size = left_part_size + right_part_size;
-    int* array = calloc(array_size, sizeof(int));
-    for (int i = 0; i < array_size; i++) {
+    int arraySize = leftPartSize + rightPartSize;
+    int* array = calloc(arraySize, sizeof(int));
+    for (int i = 0; i < arraySize; i++) {
         printf("[%d]=", i);
         scanf("%d", &array[i]);
     }
 
-    swap_array_parts(left_part_size, right_part_size, array);
+    swapArrayParts(leftPartSize, rightPartSize, array);
 
     printf("Array with swapped parts: ");
-    for (int i = 0; i < array_size; i++) {
+    for (int i = 0; i < arraySize; i++) {
         printf("%d ", array[i]);
     }
 
