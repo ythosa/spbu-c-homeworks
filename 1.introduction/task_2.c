e85abdc09f5a0e14103bbe6fecd86bf8cf306597
Ythosa <vasus714@yandex.ru>
2021-09-14 15:22:24 +0300

    fix: naming

--- 1.introduction/task_2.c
+++ 1.introduction/task_2.c
@@ -2,17 +2,17 @@
 #include <malloc.h>
 #include <stdio.h>
 
-int find_max_element_occurring_more_than_once(const int* array, int array_size)
+int findMaxElementOccurringMoreThanOnce(const int* array, int arraySize)
 {
-    int* occurred_times = calloc(array_size, max(array, array_size) * sizeof(int));
+    int* occurredTimes = calloc(arraySize, max(array, arraySize) * sizeof(int));
 
-    for (int i = 0; i < array_size; i++) {
-        occurred_times[array[i]] += 1;
+    for (int i = 0; i < arraySize; i++) {
+        occurredTimes[array[i]] += 1;
     }
 
     int m = 0;
-    for (int i = 0; i < array_size; i++) {
-        if (array[i] > m && occurred_times[array[i]] > 1) {
+    for (int i = 0; i < arraySize; i++) {
+        if (array[i] > m && occurredTimes[array[i]] > 1) {
             m = array[i];
         }
     }
@@ -22,17 +22,17 @@ int find_max_element_occurring_more_than_once(const int* array, int array_size)
 
 int main()
 {
-    int array_size = 0;
+    int arraySize = 0;
     printf("Input array size:");
-    scanf("%d", &array_size);
+    scanf("%d", &arraySize);
 
-    int* array = calloc(array_size, sizeof(int));
-    for (int i = 0; i < array_size; i++) {
+    int* array = calloc(arraySize, sizeof(int));
+    for (int i = 0; i < arraySize; i++) {
         printf("[%d]=", i);
         scanf("%d", &array[i]);
     }
 
-    int m = find_max_element_occurring_more_than_once(array, array_size);
+    int m = findMaxElementOccurringMoreThanOnce(array, arraySize);
     printf("Maximum element occurring more than once is %d", m);
 
     free(array);
