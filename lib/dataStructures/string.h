3312228a37793df7db481cd38e02818b88830632
ythosa <vasus714@yandex.ru>
2021-09-27 17:47:05 +0300

    docs: string lib

--- lib/dataStructures/string.h
+++ lib/dataStructures/string.h
@@ -6,16 +6,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* A String is a reference type.
+ * You should use stringFree to free up the string memory. */
 typedef struct string* String;
 
+/* Free up string memory. */
 void stringFree(String s);
+
+/* Returns string length. */
 uint8_t stringLength(String s);
+
+/* Prints string into `dst` output. */
 void stringPrint(String s, FILE* dst);
+
+/* Constructs string from char*.
+ * Analog for `strdup`. */
 String stringDup(const char* from);
+
+/* Converts string to char*. */
 uint8_t* stringC(String s);
+
+/* Adds a character to the end of the string. */
 String stringPushChar(String s, char c);
+
+/* Performs string concatenation. */
 String stringConcat(String left, String right);
+
+/* Returns char element with index from string.
+ * There is no check for the validity of the index. */
 uint8_t stringGetByIndex(String s, uint8_t index);
+
+/* Compares two strings lexicographically.
+ * `strcmp` analog. */
 int stringCompare(String s1, String s2);
 
 #endif // SPBU_C_HOMEWORKS_STRING_H
