18404a1a0f2f6534088206dd45301f40e3411b40
ythosa <vasus714@yandex.ru>
2021-09-27 17:51:27 +0300

    docs: list lib

--- lib/dataStructures/list.h
+++ lib/dataStructures/list.h
@@ -12,11 +12,21 @@ typedef struct list {
     int size;
 } list_t;
 
+/* A List is a reference type.
+ * You should use listFree to free up the string memory. */
 typedef struct list* List;
 
+/* Creates empty list. */
 List listCreate();
+
+/* Free up list memory. */
 void listFree(List l);
+
+/* Adds an element to the end of the list. */
 void listPushBack(List l, void* data);
+
+/* Prints list to `dst` output in format "<e1> <e2>...".
+ * You need to provide a function that formats element to string. */
 void listPrint(List l, String (*elementFormatter)(void*), FILE* dst);
 
 #endif // SPBU_C_HOMEWORKS_LIST_H
