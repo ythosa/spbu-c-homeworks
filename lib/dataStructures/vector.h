ded29ca53b79dcffd2456b6238b774f4ec84a849
Ythosa <vasus714@yandex.ru>
2021-10-05 13:01:49 +0300

    refactor

---
+++ lib/dataStructures/vector.h
@@ -0,0 +1,16 @@
+#ifndef SPBU_C_HOMEWORKS_VECTOR_H
+#define SPBU_C_HOMEWORKS_VECTOR_H
+
+typedef struct Vector Vector;
+
+Vector* vectorCreate();
+void vectorFree(Vector* vector);
+int vectorSize(Vector* vector);
+void vectorPushback(Vector* vector, int data);
+void vectorPrint(Vector* vector, FILE* destination);
+double vectorLength(Vector* vector);
+Vector* vectorPlus(Vector* leftOperand, Vector* rightOperand);
+Vector* vectorMinus(Vector* leftOperand, Vector* rightOperand);
+bool vectorDotProduct(int* result, Vector* leftOperand, Vector* rightOperand);
+
+#endif // SPBU_C_HOMEWORKS_VECTOR_H
