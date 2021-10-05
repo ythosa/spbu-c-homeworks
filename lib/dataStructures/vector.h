e8621dc6d8500a6c8f26c3f91c503681b7bad328
Ythosa <vasus714@yandex.ru>
2021-10-05 13:10:59 +0300

    fix: includes

--- lib/dataStructures/vector.h
+++ lib/dataStructures/vector.h
@@ -1,7 +1,10 @@
 #ifndef SPBU_C_HOMEWORKS_VECTOR_H
 #define SPBU_C_HOMEWORKS_VECTOR_H
 
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Vector Vector;
 
