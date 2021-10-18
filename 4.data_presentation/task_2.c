72643e8d1002c6e20ef8bab3108313e18b87d477
ythosa <vasus714@yandex.ru>
2021-10-19 02:08:34 +0300

    feat: task2 solve with correct task understanding

--- 4.data_presentation/task_2.c
+++ 4.data_presentation/task_2.c
@@ -1,5 +1,76 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct NormalizedView {
+    bool sign;
+    double fraction;
+    int base;
+    int exponent;
+} NormalizedView;
+
+NormalizedView* normalizedViewCreate(bool sign, double fraction, int base, int exponent)
+{
+    NormalizedView* normalizedView = malloc(sizeof(NormalizedView));
+
+    normalizedView->sign = sign;
+    normalizedView->fraction = fraction;
+    normalizedView->base = base;
+    normalizedView->exponent = exponent;
+
+    return normalizedView;
+}
+
+void normalizedViewFree(NormalizedView* normalizedView)
+{
+    free(normalizedView);
+}
+
+void normalizedViewPrint(NormalizedView* normalizedView, FILE* destination)
+{
+    fprintf(
+        destination,
+        "%s%.17f * %d^%d\n",
+        normalizedView->sign ? "-" : "+",
+        normalizedView->fraction,
+        normalizedView->base,
+        normalizedView->exponent);
+}
+
+void doubleToBinary(double number, char binary[64])
+{
+    u_int64_t bits = 0;
+    memcpy(&bits, &number, sizeof(double));
+    for (int i = 63; i >= 0; i--)
+        binary[63 - i] = (bits >> i) & 1;
+}
+
+NormalizedView* normalizedViewForDouble(double number)
+{
+    char binary[64] = "";
+    doubleToBinary(number, binary);
+
+    double powerOfTwo = 1024;
+    int exponent = -1023;
+    for (int i = 1; i < 12; i++) {
+        if (binary[i])
+            exponent += (int)powerOfTwo;
+        powerOfTwo /= 2;
+    }
+
+    double fraction = 1;
+    for (int i = 12; i < 64; ++i) {
+        if (binary[i])
+            fraction += powerOfTwo;
+        powerOfTwo /= 2;
+    }
+
+    if (exponent == -1023 && fraction == 1)
+        return normalizedViewCreate(binary[0], 0, 2, 0);
+
+    return normalizedViewCreate(binary[0], fraction, 2, exponent);
+}
 
 int main()
 {
@@ -7,13 +78,8 @@ int main()
     printf("Input number: ");
     scanf("%lf", &number);
 
-    int integerPartOfNumber = (int)number;
-    int powerOf2 = 0;
-    while (integerPartOfNumber >= 2) {
-        number /= 2;
-        integerPartOfNumber = abs((int)number);
-        powerOf2 += 1;
-    }
-
-    printf("%.30lf*2^%d", number, powerOf2);
+    NormalizedView* normalizedView = normalizedViewForDouble(number);
+    printf("Normalized view: ");
+    normalizedViewPrint(normalizedView, stdout);
+    normalizedViewFree(normalizedView);
 }
