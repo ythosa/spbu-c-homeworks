146bd8f45440af80f523ff722a2fd008e743cb2e
ythosa <vasus714@yandex.ru>
2021-10-20 04:10:18 +0300

    refactor: task6.2 with requirements :D

--- 6.complexity_and_sorting/task_1.c
+++ 6.complexity_and_sorting/task_1.c
@@ -1,51 +1,27 @@
-#include <string.h>
+#include "../lib/mergeSort/mergeSort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#define COPY_ARRAY(destination, source, length, sizeOfElement) (memcpy(destination, source, length * sizeOfElement))
-
-void mergeSubarrays(int array[], int leftIndex, int middleIndex, int rightIndex)
+int main()
 {
-    int sizeOfLeftSubarray = middleIndex - leftIndex + 1;
-    int sizeOfRightSubarray = rightIndex - middleIndex;
+    printf("Input array size: ");
+    int arraySize = 0;
+    scanf("%d", &arraySize);
+    if (arraySize < 0) {
+        printf("Invalid array size. Must be > 0");
 
-    int leftSubarray[sizeOfLeftSubarray], rightSubarray[sizeOfRightSubarray];
-    COPY_ARRAY(leftSubarray, array + leftIndex, sizeOfLeftSubarray, sizeof(int));
-    COPY_ARRAY(rightSubarray, array + middleIndex + 1, sizeOfRightSubarray, sizeof(int));
-
-    int leftSubarrayIndex = 0;
-    int rightSubarrayIndex = 0;
-    int baseArrayIndex = leftIndex;
-    while (leftSubarrayIndex < sizeOfLeftSubarray && rightSubarrayIndex < sizeOfRightSubarray) {
-        if (leftSubarray[leftSubarrayIndex] <= rightSubarray[rightSubarrayIndex]) {
-            array[baseArrayIndex] = leftSubarray[leftSubarrayIndex];
-            leftSubarrayIndex++;
-        } else {
-            array[baseArrayIndex] = rightSubarray[rightSubarrayIndex];
-            rightSubarrayIndex++;
-        }
-        baseArrayIndex++;
+        return 0;
     }
 
-    while (leftSubarrayIndex < sizeOfLeftSubarray) {
-        array[baseArrayIndex] = leftSubarray[leftSubarrayIndex];
-        leftSubarrayIndex++;
-        baseArrayIndex++;
-    }
+    printf("Input array: ");
+    int* array = malloc(arraySize * sizeof(int));
+    for (int i = 0; i < arraySize; i++)
+        scanf("%d", &array[i]);
 
-    while (rightSubarrayIndex < sizeOfRightSubarray) {
-        array[baseArrayIndex] = rightSubarray[rightSubarrayIndex];
-        rightSubarrayIndex++;
-        baseArrayIndex++;
-    }
-}
+    sort(array, arraySize);
+    printf("Sorted array: ");
+    for (int i = 0; i < arraySize; i++)
+        printf("%d ", array[i]);
 
-void mergeSort(int array[], int leftIndexOfSubarray, int rightIndexOfSubarray)
-{
-    if (leftIndexOfSubarray >= rightIndexOfSubarray) {
-        return;
-    }
-
-    int middle = leftIndexOfSubarray + (rightIndexOfSubarray - leftIndexOfSubarray) / 2;
-    mergeSort(array, leftIndexOfSubarray, middle);
-    mergeSort(array, middle + 1, rightIndexOfSubarray);
-    mergeSubarrays(array, leftIndexOfSubarray, middle, rightIndexOfSubarray);
+    return 0;
 }
