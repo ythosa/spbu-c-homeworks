b948eae9436e2c538e4b5b342dadff314997428a
ythosa <vasus714@yandex.ru>
2021-12-18 16:42:57 +0300

    fix: linter erros

--- retest1/task_2.c
+++ retest1/task_2.c
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
     countLetters(counter, inputFile);
     printResult(counter, outputFile);
     freeLatinLowercaseLettersCounter(counter);
-    
+
     fclose(inputFile);
     fclose(outputFile);
 
